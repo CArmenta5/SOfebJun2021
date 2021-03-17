d8fec5ed4e62acae10a1f654dab5e67751c143b0
A01635371 Carlos <carlosraulap@outlook.com>
2021-03-17 01:27:15 +0000

    Practica 2

--- xv6-public/init.c
+++ xv6-public/init.c
@@ -20,13 +20,16 @@ main(void)
   dup(0);  // stderr
 
   for(;;){
-    printf(1, "init: starting sh\n");
+	// 3) Corre perfectamente, pero ahora solo cambia el mensaje cuando comienza el sh por el nuevo mensaje"
+    printf(1, "init: starting sh wow Supercalifragilisticexpialidocious\n");
     pid = fork();
     if(pid < 0){
       printf(1, "init: fork failed\n");
       exit();
     }
     if(pid == 0){
+		// 1) En la primera modificación existe un loop infinito donde muestra el directorio y muestra todos los archivos que se pueden ejecutar.
+		//2) En la segunda modificación el init intenta ejecutar el sh, el exec regresa el fail y el init manda un mensaje que dice que el exec sh ha fallado"
       exec("sh", argv);
       printf(1, "init: exec sh failed\n");
       exit();
