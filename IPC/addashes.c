19cab592699ce56c7f0be67f5903acb5520543c9
CArmenta5 <carlosraulap@outlook.com>
2021-04-20 17:21:11 +0000

    Notas de estudios de pipes

--- IPC/addashes.c
+++ IPC/addashes.c
@@ -2,25 +2,25 @@
 #include <stdio.h>
 
 int main(){
-    //int pid =fork()
-   // execl("/usr/bin/tr","ls","-l",NULL);
+    // execl("/usr/bin/ls","ls","-l",NULL); esta llamada al sistema cambiaría todo nuestro código
     int fd[2];
     int pid;
-    pipe(fd);
-    pid=fork(); //Dos procesos
-    if(pid==0){
-        close(fd[1]); //cerramos la parte padre
-        //fd[1] salida de mi piope
-        dup2(fd[0],STDIN_FILENO);//conectamos salida de cat a fd 0
+    pipe(fd); // llama la sistema y crea la tuberia
+    pid = fork(); // proceso clon creado como hijo
+    if(pid == 0){
+        // HIJO
+        close(fd[1]); // cerramos la otra conexión que no vamos a utilizar en el proceso hijo (entrada)
+        // fd[1] salida de mi piope
+        dup2(fd[0],STDIN_FILENO); // la salida la conectamos con la entrada de tr
         
-        execl("/usr/bin/tr","tr","' '","-",NULL);
+        // antes de exec hay que conectar entradas y salidas
+        execl("/usr/bin/tr","tr","' '","-",NULL); // exec s
     }
     if(pid > 0){
-        //padre
-        close(fd[0]); //cerramos la parte padre
-        //fd[1] salida de mi piope
-        dup2(fd[1],STDOUT_FILENO);//conectamos salida de cat a fd 0
-        
+        // PADRE
+        close(fd[0]); // cerramos la otra conexión que no vamos a utilizar en el proceso padre (salida)
+        // fd[1] salida de mi piope
+        dup2(fd[1],STDOUT_FILENO); // la entrada la conectamos con la salida de cat        
         execl("/usr/bin/cat","cat",NULL);
     }
     printf("Esto no debe verse \n");
